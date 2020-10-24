7367a47d6d7857896e497d0fe7c68fa80b822e64
Torbjörn Rathsman <milasudril@gmail.com>
2020-10-24 09:58:45 +0200

    Move client code to testcase

--- libenum/variant.hpp
+++ libenum/variant.hpp
@@ -24,30 +24,3 @@ namespace Library
     template<class EnumType, template<EnumType> class EnumItemTraits>
     using Variant = typename detail::make_variant<EnumType, EnumItemTraits>::type;
 }
-
-
-#include <cstdint>
-namespace Client
-{
-    enum class TypeId:uint8_t{Int32, Float32};
-    constexpr auto end(Library::Empty<TypeId>){return static_cast<std::underlying_type_t<TypeId>>(TypeId::Float32) + 1;}
-
-    template<TypeId n>
-    struct TypeInfo;
-
-    template<>
-    struct TypeInfo<TypeId::Int32>
-    {
-        using type = int32_t;
-        static constexpr char const* name = "i32";
-    };
-
-    template<>
-    struct TypeInfo<TypeId::Float32>
-    {
-        using type = float;
-        static constexpr char const* name = "f32";
-    };
-
-    using MyVariant = Library::Variant<TypeId, TypeInfo>;
-}
